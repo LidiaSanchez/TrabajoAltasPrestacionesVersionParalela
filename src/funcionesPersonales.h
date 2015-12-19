e95407cefc966685ef6541dc6539915282f3b22d
Cristina Blanco <cblanf01@estudiantes.unileon.es>
2015-12-19 11:32:25 +0100

    Create funcionesPersonales.h

---
+++ src/funcionesPersonales.h
@@ -0,0 +1,9 @@
+ #ifndef FUNCIONESPERSONALES_H
+ #define FUNCIONESPERSONALES_H
+ 
+ void leeEntero(FILE*,int*);
+ void leeDouble(FILE*,double*);
+ void leeCadena(FILE*,char*);
+ void leeLinea(FILE*);
+ 
+ #endif /* FUNCIONESPERSONALES_H_ */

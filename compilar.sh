4b28c9f579dbfd2dbe11fadca9bc99874cf95800
Iván <ipazce00@estudiantes.unileon.es>
2015-12-20 02:34:59 +0100

    Modificado script de compilación para compilar la solución entera. ()

--- compilar.sh
+++ compilar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -o solidos src/solidos.c -lm
+gcc -o solidos src/variables.c src/solidos.c src/funcionesPersonales.c src/funcionesD.c src/funcionesA.c src/funcionesMain.c src/funcionesB.c src/funcionesC.c -lm
